fa55ae5fe9bf215184f1ad29cd2fe22462104c6b
Nkennyelvis <e.nsengiman@alustudent.com>
2025-02-26 02:26:56 -0800

    tasks

---
+++ addnumbersfunction.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+#Ask user for input
+num_a = int(input("Enter the first number: \n"))
+num_b = int(input("Enter the second number:"))
+
+print (num_a + num_b)
---
+++ helloworld.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Replace with your name if needed
+
+print("Hello, World!")
+name= input("Enter your name:")
+print (f"Hello, {name}")
+
